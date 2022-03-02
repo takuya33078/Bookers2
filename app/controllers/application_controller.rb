a90860b5116d9196256f748c227e8d93bae9179f
takuya33078 <ec2-user@ip-172-31-13-64.ec2.internal>
2022-03-02 04:17:51 +0000

    Bookers２課題

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   add_flash_types :success, :info, :warning, :danger
 
   private
@@ -16,5 +16,5 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
-  
+
 end
